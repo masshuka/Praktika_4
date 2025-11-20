1a15c1f1d046996b0930a6758bf05efce1723918
Masshh <msipicyna96@gmail.com>
2025-11-20 23:08:41 +0500

    Созданный проект

---
+++ Common/Classes/ViewModelSend.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+   public class ViewModelSend
+    {
+        public string Message { get; set; }
+
+        public int Id { get; set; }
+        public ViewModelSend(string message, int id)
+        {
+            this.Message = message;
+            this.Id = Id;
+        }
+    }
+}
