06ba28e34797d2a50cad55f4f92ce8de5efb9cd4
Masshh <msipicyna96@gmail.com>
2025-11-20 23:19:04 +0500

    Созданы классы в проекте «Common»

---
+++ Common/Classes/ViewModelMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class ViewModelMessage
+    {
+        public string Command { get; set; }
+        public string Data { get; set; }
+        public ViewModelMessage(string command, string data)
+
+        {
+            this.Command = Command;
+            this.Data = Data;
+        }
+    }
+}
