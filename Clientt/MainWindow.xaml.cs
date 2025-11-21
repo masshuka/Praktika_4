98226959ab1a9007bf1449d5c46ca152b148fad6
csamk <c.samkov@gmail.com>
2025-11-21 10:36:25 +0300

    прописана функционал кнопки btnconnect для проверки подключения

--- Clientt/MainWindow.xaml.cs
+++ Clientt/MainWindow.xaml.cs
@@ -1,18 +1,15 @@
-﻿using System;
+﻿using Common;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Diagnostics;
+using System.IO;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Clientt
 {
@@ -28,6 +25,47 @@ namespace Clientt
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            if (IPAddress.TryParse(txtIpAddress.Text, out ipAddress) && int.TryParse(txtPort.Text, out port))
+            {
+                string login = txtLogin.Text;
+                string password = txtPassword.Password;
+                if (string.IsNullOrWhiteSpace(login) || string.IsNullOrWhiteSpace(password))
+                {
+                    MessageBox.Show("Введите логин и пароль.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    return;
+                }
+                try
+                {
+                    var response = SendCommand($"connect {login} {password}");
+                    if (response?.Command == "autorization")
+                    {
+                        userId = int.Parse(response.Data);
+                        MessageBox.Show("Подключение успешно!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                        LoadDirectories();
+                    }
+                    else
+                    {
+                        MessageBox.Show(response?.Data ?? "Ошибка авторизации.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка подключения: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите корректный IP и порт.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void Download(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
